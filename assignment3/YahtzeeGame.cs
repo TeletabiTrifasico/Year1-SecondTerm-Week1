e03a468573858f0b9b38113ae75f21803eb98536
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 18:28:19 +0100

    Add files via upload

---
+++ assignment3/YahtzeeGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    internal class YahtzeeGame
+    {
+        Dice[] dices;
+        public YahtzeeGame()
+        {
+            dices = new Dice[5];
+            for (int i = 0; i < dices.Length; i++)
+            {
+                dices[i] = new Dice();
+            }
+        }
+        public void Throw()
+        {
+            for(int i = 0; i < dices.Length; i++)
+            {
+                dices[i].Throw();
+            }
+        }
+        public void DisplayValues()
+        {
+            for (int i = 0; i < dices.Length; i++)
+            {
+                dices[i].DisplayValue();
+            }
+            Console.WriteLine();
+        }
+        public bool Yahtzee()
+        {
+            int first = dices[0].value;
+            for (int i = 1; i < dices.Length; i++)
+            {
+                if (dices[i].value != first)
+                { 
+                    return false; 
+                }
+            }
+            return true;
+        }
+    }
+}
