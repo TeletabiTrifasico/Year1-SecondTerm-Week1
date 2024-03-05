f533cf0dc87801da8584d9021f9bc5194259be5a
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 18:27:10 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            PrintMonth();
+            Console.WriteLine();
+            Console.Write(ReadMonth("Enter a month number: "));
+        }
+        void PrintMonth(Months month)
+        {
+            Console.WriteLine($"{(int)month,2}. {month}");
+        }
+        void PrintMonth()
+        {
+            for (int i = 1; i<= 12; i++)
+            {
+                Months currentMonth = new Months();
+                currentMonth = (Months)i;
+                PrintMonth(currentMonth);
+            }
+        }
+        Months ReadMonth(string question)
+        {
+            do
+            {
+                Console.Write(question);
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (Enum.IsDefined(typeof(Months), number))
+                {
+                    Console.Write(number + " => ");
+                    return (Months)number;
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is not a valid value.");
+                }
+            }while (true);
+        }
+    }
+}
